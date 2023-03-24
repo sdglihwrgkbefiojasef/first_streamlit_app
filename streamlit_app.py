c1c4cc6ae7a5339dbab59edb545967f9cd12b409
sdglihwrgkbefiojasef <128615093+sdglihwrgkbefiojasef@users.noreply.github.com>
2023-03-24 14:11:26 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -22,7 +22,6 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 # Display the table on the page.
 streamlit.dataframe(fruits_to_show)
 
-
 #New section to define function
 def get_fruityvice_data(this_fruit_choice):
    fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+this_fruit_choice)
@@ -46,13 +45,28 @@ streamlit.header("The fruit load list contains:")
 #snowflake related functions
 def get_fruit_load_list():
    with my_cnx.cursor() as my_cur:
-      my_cur.execute("select * from pc_rivery_db.public.fruit_load_list")
+      
       return my_cur.fetchall()
    
 #add a button to load the fruit
 if streamlit.button('Get Fruit Load List'):
-   my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+   
    my_data_rows = get_fruit_load_list()
    streamlit.dataframe(my_data_rows)
 
 streamlit.stop()
+
+
+#Allow the end user to add to the list 
+def insert_row_snowflake(new_fruit):
+   with my_cnx.curser() as my_cur:
+      my_cur.execute("select * from pc_rivery_db.public.fruit_load_list")
+      return "Thanks for adding" + new_fruit
+   
+add_my_fruit = streamlit.text_input('What fruit would you like to add?')
+if streamlit.button('Add fruit to list'):
+   my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+   back_from_function = insert_row_snowflake(add_my_fruit)
+   streamlit.text(back_from_function)
+
+
