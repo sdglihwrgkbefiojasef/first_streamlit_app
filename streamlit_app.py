ebc0f6681643f4fa3421f21bde671f0d06102997
sdglihwrgkbefiojasef <128615093+sdglihwrgkbefiojasef@users.noreply.github.com>
2023-03-24 09:44:23 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -22,17 +22,23 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 # Display the table on the page.
 streamlit.dataframe(fruits_to_show)
 
+
+#New section for Fruit Advice I think lol...
 streamlit.header("Fruityvice Fruit Advice!")
-fruit_choice = streamlit.text_input('What fruit would you like information about?')
-streamlit.write('The user entered ', fruit_choice)
+try:
+  fruit_choice = streamlit.text_input('What fruit would you like information about?')
+  if not fruit_choice:
+    streamlit.error("Please select a fruit to get information")
+    else:
+      fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+fruit_choice)
+      fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+      streamlit.dataframe(fruityvice_normalized)
 
 
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+fruit_choice)
+except URLError as e:
+  streamlit.error()
 
-# normalize data to a more readable format
-fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-# displays table
-streamlit.dataframe(fruityvice_normalized)
+
 
 streamlit.stop()
 
